039229fe717831d361a28466a53f730c2db5d5db
jentsanblackbaud <40302364+jentsanblackbaud@users.noreply.github.com>
2018-06-19 15:04:08 -0400

    Added Script to Download SOAP Endpoints
    This script will read in a file that will specify which endpoints to download and where to save them to

---
+++ EndpointDownload.ps1
@@ -0,0 +1,39 @@
+﻿#read in the config file to figure out which soap endpoints to download, there may be multiple will have to loop through
+
+#read config file
+$configPath = "C:\Users\jennifer.tsan\Documents"
+$fileName ="\EndpointDownloadConfig.csv"
+$fullPath = $configPath + $fileName
+$fileContent = Import-Csv $fullPath
+
+foreach ($endpoint in $fileContent) {
+    #get endpoint name, get location (5 possible locations)
+    $endpointName = $endpoint.EndpointName.ToLower() -replace '\s',''
+    $endpointLocation = $endpoint.EndpointLocation.ToUpper() -replace '\s',''
+    $credential = $endpoint.Credential
+    $outputPath = $endpoint.SaveLocation
+
+    If($endpointLocation -like "*creates*") {
+        $endpointLocation = "recordadds"
+    } ElseIf ($endpointLocation -like "*updates*") {
+        $endpointLocation = "recordedits"
+    } ElseIf ($endpointLocation -like "*operations*" -or $endpointLocation -like "*deletes*") {
+        $endpointLocation = "recordoperations"
+    } ElseIf ($endpointLocation -like "*searches*") {
+        $endpointLocation = "searchlists"
+    }
+
+    #get location to save file
+    $saveLocation = $endpoint.SaveLocation
+
+    $url = "http://bbcrm.eastus.cloudapp.azure.com/bbappfx/vpp/bizops/db[BBINFINITY]/" + $endpointLocation + "/" + $endpointName + "/soap.asmx?WSDL"
+
+    [string]$outputPath = $outputPath + "\" + $endpointName + ".swagger"
+
+    $url
+    $outputPath
+
+    Invoke-WebRequest -Uri $url -outFile $outputPath -Credential $credential
+}
+
+
